91a9b00137f114cdbd7c905164ab28b68b35db4c
schoenfelde <schoenfeld.elliot@yahoo.com>
2016-11-17 22:15:41 -0500

    Add safetycheck.js and fix package.json prob

---
+++ SafetyCheck.js
@@ -0,0 +1,12 @@
+//This file will serve as some extra server side validation for Drugs and Drug amounts
+"use strict";
+
+//Drug object will contain valid names and also valid amounts
+//e.g. drug : Insulin , min: 0 mL, max: 100mL, overTime: Y 
+
+//Posible Drugs: Saline, Dextrose, Harttmans, Ringers ?
+
+
+
+var  = [];
+
